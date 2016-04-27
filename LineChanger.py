b479159d9414f1b92fe444a56d7c02b3cbc72c75
Brien Blandford <brien.blandford@gmail.com>
2016-04-27 15:37:07 -0400

    make commit for tabular version
    this version allows there to be a tab after the initial
    line number, instead of what used to be 2 spaces.
    i found that when i was trying to import this into the siemens system, it was giving me a lot of werrors with
    spaces.  the tabs work fine, and it is the way the code
    downloads anyway

--- LineChanger.py
+++ LineChanger.py
@@ -76,14 +76,14 @@ class AdjustLineNumsCommand(sublime_plugin.TextCommand):
 		GO_map = {}
 		for i, line in enumerate(content.split('\n')):
 			try:
-				lineNum = self.add_leading_zeroes(re.search(r'(^[0-9]+)([ ]+)',
+				lineNum = self.add_leading_zeroes(re.search(r'(^[0-9]+)([\t])',
 													line).group(1))
 			except:
 				pass
 			lineNumReplace = self.add_leading_zeroes(int(self.start) +
 													 (i * int(self.increment)))
 			if lineNum == None:
-				line = lineNumReplace + '  ' + line
+				line = lineNumReplace + '\t' + line
 			else:
 				if lineNum in GOs:
 					GO_map[lineNum] = self.add_leading_zeroes(lineNumReplace)
@@ -160,21 +160,22 @@ class InsertLinesCommand(sublime_plugin.TextCommand):
 		currentLine = self.view.substr(self.view.line(self.view.sel()[0]))
 		rowandcol = self.view.rowcol(self.view.sel()[0].begin())
 		if (int(rowandcol[0]) == 0) and (currentLine == ''):
-			self.view.insert(edit, self.view.line(self.view.sel()[0]).end(),'01000  ')
+			self.view.insert(edit, self.view.line(self.view.sel()[0]).end(),'01000\t')
 
 		(row, col) = (int(rowandcol[0]) + 1, int(rowandcol[1])+1)
 		nextLine = self.view.substr(self.view.line(sublime.Region(self.view.text_point(row, 0))))
 
 		try:
-			lineNum = self.add_leading_zeroes(re.search(r'(^[0-9]+)([ ]+)',
+			lineNum = self.add_leading_zeroes(re.search(r'(^[0-9]+)([\t])',
 												currentLine).group(1))
 		except:
 			lineNum = None
 
 		try:
-			nextLineNum = re.search(r'(^[0-9]+)([ ]+)', nextLine).group(1)
+			nextLineNum = re.search(r'(^[0-9]+)([\t])', nextLine).group(1)
 			nextLineNum = self.add_leading_zeroes(nextLineNum)
 		except:
+			# why did i make this a space?
 			nextLineNum = ' '
 
 		if lineNum is not None:
@@ -186,7 +187,7 @@ class InsertLinesCommand(sublime_plugin.TextCommand):
 		# print (self.view.rowcol(self.view.size())[0], row)
 		if (int(newLineNum) < int(nextLineNum)) or (self.view.rowcol(self.view.size())[0] == row-1):
 			self.view.insert(edit, self.view.line(self.view.sel()[0]).end(),
-							'\n'+str(newLineNum)+'  ')
+							'\n'+str(newLineNum)+'\t')
 
 
 	def add_leading_zeroes(self, linenum):
@@ -207,7 +208,7 @@ class InsertLinesCommand(sublime_plugin.TextCommand):
 
 class InsertCommentCommand(sublime_plugin.TextCommand):
 	'''
-	This command will insert a comment (01000  C ) when ctrl+/ is pressed over all
+	This command will insert a comment (01000 tab C ) when ctrl+/ is pressed over all
 	the selected lines. 
 	'''
 	def run(self, edit):
@@ -239,6 +240,7 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
 					line = self.view.substr(self.view.line(
 							sublime.Region(self.view.text_point(row, 0))))
 					commentedLine = self.toggle_comment(line)
+					print (commentedLine)
 					self.view.replace(edit, self.view.line(
 							sublime.Region(self.view.text_point(row, 0))), commentedLine)
 
@@ -248,11 +250,16 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
 		given a line, toggle the comment C at the beginning of the line.
 		'''
 		try:
-			lineNum = re.search(r'(^[0-9]+)([ ]+)(C)?', line)
+			lineNum = re.search(r'(^[0-9]+)([\t])(C ?)?', line)
 			# print (lineNum.groups())
-			if lineNum.groups()[-1] == 'C':
-				return line.replace('C', '')
+			if (lineNum.groups()[-1] == 'C') or (lineNum.groups()[-1] == 'C '):
+				tempstring = ''
+				for group in lineNum.groups():
+					tempstring += str(group)
+				# print (tempstring.replace('C', ''))
+				return (line.replace(tempstring, tempstring.replace('C ', '').replace('C', '')))
+				# return line.strip(tempstring, 'C ').strip(tempstring, 'C')
 			else:
-				return line.replace(lineNum.groups()[0] + '  ', lineNum.groups()[0] + '  C ')
+				return (line.replace(lineNum.groups()[0] + '\t', lineNum.groups()[0] + '\tC '))
 		except:
 			pass
