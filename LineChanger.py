76905f2a1877195384abf9f8fe9e717dd4a60117
Brien Blandford <brien.blandford@gmail.com>
2016-04-27 08:52:35 -0400

    make commit for Princeton University Sharing
    this commit is for the package that will be installed on the PU
    server for the plant operators (and me) to use.
    
    this includes the
    	- PPCL YAML file
    	- the PPCL tmLanguage file
    	- the LineChanger.py file
    	- the key mappings file
    
    I will also download package manager for sublime text and then the plugin
    for insert nums

--- LineChanger.py
+++ LineChanger.py
@@ -19,35 +19,61 @@ class AdjustLineNumsCommand(sublime_plugin.TextCommand):
 		# get the entire content of the code
 		content = self.view.substr(sublime.Region(0, self.view.size()))
 		# get the list of any GOTO statements
-		GOTO_list = self.get_GOTO(content)
-		newcontent = self.replace_line_nums(content, GOTO_list)
+		GO_list = self.get_GOs(content)
+		# GOSUB_list = self.get_GOSUB(content)
+		newcontent = self.replace_line_nums(content, GO_list)
+		# print ('1st pass **********\n', content, '\n**********\n')
+		# newcontent = self.replace_line_nums(newcontent, GOSUB_list)
 
 		selections = sublime.Region(0, self.view.size())
 		self.view.replace(edit, selections, newcontent)
 
 
-	def get_GOTO(self, content):
+	def get_GOs(self, content):
 		'''
-		Get all the GOTO numbers in the document,
+		Get all the GOs numbers in the document,
 		to make sure those are changed appropriately.
 		'''
-		GOTOs = []
+		GOs = []
 		for i, line in enumerate(content.split('\n')):
-			GOTO_nums = re.search(r'(GOTO )([0-9]+)', line)
+			GO_nums = re.search(r'(GOTO )([0-9]+)', line)
 			try:
-				GOTOs.append(self.add_leading_zeroes(GOTO_nums.group(2)))
+				GOs.append(self.add_leading_zeroes(GO_nums.group(2)))
 			except:
 				pass
-		return GOTOs
+			GO_nums = re.search(r'(GOSUB )([0-9]+)', line)
+			try:
+				print (GO_nums.groups())
+				GOs.append(self.add_leading_zeroes(GO_nums.group(2)))
+			except:
+				pass
+		return GOs
 
 
-	def replace_line_nums(self, content, GOTOs):
+	def get_GOSUB(self, content):
+		'''
+		Get all the GOSUB numbers in the document,
+		to make sure those are changed appropriately.
+		'''
+		GOSUBs = []
+		for i, line in enumerate(content.split('\n')):
+			GOSUB_nums = re.search(r'(GOSUB )([0-9]+)', line)
+			try:
+				GOSUBs.append(self.add_leading_zeroes(GOSUB_nums.group(2)))
+			except:
+				pass
+		# for item in GOSUBs:
+			# print (item)
+		return GOSUBs
+
+
+	def replace_line_nums(self, content, GOs):
 		'''
 		Replace all the content with the new line numbers, and return the updated content
-		and GOTO replacements.
+		and GOTO and GOSBU replacements.
 		'''
 		newcontent = ''
-		GOTO_map = {}
+		GO_map = {}
 		for i, line in enumerate(content.split('\n')):
 			try:
 				lineNum = self.add_leading_zeroes(re.search(r'(^[0-9]+)([ ]+)',
@@ -59,8 +85,8 @@ class AdjustLineNumsCommand(sublime_plugin.TextCommand):
 			if lineNum == None:
 				line = lineNumReplace + '  ' + line
 			else:
-				if lineNum in GOTOs:
-					GOTO_map[lineNum] = self.add_leading_zeroes(lineNumReplace)
+				if lineNum in GOs:
+					GO_map[lineNum] = self.add_leading_zeroes(lineNumReplace)
 				if line.startswith('0'):
 					line = line.replace(str(lineNum), str(lineNumReplace))
 				else:
@@ -71,11 +97,15 @@ class AdjustLineNumsCommand(sublime_plugin.TextCommand):
 			else:
 				newcontent += line
 
-		for key in GOTO_map.keys():
+		for key in GO_map.keys():
 			newcontent = newcontent.replace('GOTO ' + str(key),
-											 'GOTO ' + str(GOTO_map[key]))
+											 'GOTO ' + str(GO_map[key]))
 			newcontent = newcontent.replace('GOTO ' + str(key).lstrip('0'),
-											 'GOTO ' + str(GOTO_map[key]))
+											 'GOTO ' + str(GO_map[key]))
+			newcontent = newcontent.replace('GOSUB ' + str(key),
+											 'GOSUB ' + str(GO_map[key]))
+			newcontent = newcontent.replace('GOSUB ' + str(key).lstrip('0'),
+											 'GOSUB ' + str(GO_map[key]))
 		return newcontent
 
 
@@ -115,15 +145,15 @@ class AdjustLineNumsCommand(sublime_plugin.TextCommand):
 
 		while len(str(linenum)) < 5:
 			linenum = '0' + str(linenum)
-			print (linenum)
+			# print (linenum)
 		return linenum
 
 
 class InsertLinesCommand(sublime_plugin.TextCommand):
 	'''
-	This command insert a line below the current line, in an increment defaulted to 10.
-	I'm not sure yet if I want to spend the time to have it take into consideration the
-	count if it ends up being the same as the line below it.
+	This command willinsert a line below the current line, in an increment defaulted
+	to 10. I'm not sure yet if I want to spend the time to have it take into
+	consideration the count if it ends up being the same as the line below it.
 	'''
 	def run(self, edit):
 		increment = 10
@@ -156,7 +186,7 @@ class InsertLinesCommand(sublime_plugin.TextCommand):
 		# print (self.view.rowcol(self.view.size())[0], row)
 		if (int(newLineNum) < int(nextLineNum)) or (self.view.rowcol(self.view.size())[0] == row-1):
 			self.view.insert(edit, self.view.line(self.view.sel()[0]).end(),
-							'\n'+newLineNum+'  ')
+							'\n'+str(newLineNum)+'  ')
 
 
 	def add_leading_zeroes(self, linenum):
@@ -170,5 +200,59 @@ class InsertLinesCommand(sublime_plugin.TextCommand):
 			
 		while len(str(linenum)) < 5:
 			linenum = '0' + str(linenum)
-			print (linenum)
+			# print (linenum)
 		return linenum
+
+
+
+class InsertCommentCommand(sublime_plugin.TextCommand):
+	'''
+	This command will insert a comment (01000  C ) when ctrl+/ is pressed over all
+	the selected lines. 
+	'''
+	def run(self, edit):
+		for region in self.view.sel():  
+			# Get the selected text 
+			if region.empty():
+				rows = set()
+				# this is the case where there is just a cursor on the screen,
+				# nothing selected, but there could be multiple cursors
+				for pos in self.view.sel():
+					rows.add(int(self.view.rowcol(pos.begin())[0]))
+
+				for row in rows:
+					line = self.view.substr(self.view.line(
+							sublime.Region(self.view.text_point(row, 0))))
+					commentedLine = self.toggle_comment(line)
+					self.view.replace(edit, self.view.line(
+							sublime.Region(self.view.text_point(row, 0))), commentedLine)
+				return
+
+			else:
+				# this is the case where there is a region selected
+				# this might not have selected the total lines in the
+				# selected rows, so we have to iterate over each row in the selectoin.
+				row_begin = int(self.view.rowcol(self.view.sel()[0].begin())[0])
+				row_end = int(self.view.rowcol(self.view.sel()[0].end())[0])
+				# iterate over all the rows in the selection
+				for row in range(row_begin, row_end+1):
+					line = self.view.substr(self.view.line(
+							sublime.Region(self.view.text_point(row, 0))))
+					commentedLine = self.toggle_comment(line)
+					self.view.replace(edit, self.view.line(
+							sublime.Region(self.view.text_point(row, 0))), commentedLine)
+
+
+	def toggle_comment(self, line):
+		'''
+		given a line, toggle the comment C at the beginning of the line.
+		'''
+		try:
+			lineNum = re.search(r'(^[0-9]+)([ ]+)(C)?', line)
+			# print (lineNum.groups())
+			if lineNum.groups()[-1] == 'C':
+				return line.replace('C', '')
+			else:
+				return line.replace(lineNum.groups()[0] + '  ', lineNum.groups()[0] + '  C ')
+		except:
+			pass
